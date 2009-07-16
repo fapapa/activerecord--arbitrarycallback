402771c80706dfb354d6759035063aa425bcec3e
Fabio Papa <fabio@leoneimage.com>
2009-07-15 18:19:23 -0700

    Trying some experimental code to simplify things

--- lib/arbitrary_callback.rb
+++ lib/arbitrary_callback.rb
@@ -12,10 +12,10 @@ module LeoneImage
           raise ArgumentError, "Did not receive the name of a method to append"
         end
         
-        original_subject_method = "original_#{normalize_method_name(subject_method)}"
+        original_subject_method = instance_method(subject_method)
         
         wrap_method(subject_method) do |*args|
-          result = send(original_subject_method, *args)
+          result = original_subject_method.bind(self).call(*args)
           if options[:with_args]
             send(method_to_append, *args)
           else
@@ -32,23 +32,21 @@ module LeoneImage
           raise ArgumentError, "Did not receive the name of a method to prepend"
         end
         
-        original_subject_method = "original_#{normalize_method_name(subject_method)}"
+        original_subject_method = instance_method(subject_method)
         
         wrap_method(subject_method) do |*args|
-          result = send(original_subject_method, *args)
           if options[:with_args]
             send(method_to_prepend, *args)
           else
             send(method_to_prepend)
           end
+          result = original_subject_method.bind(self).call(*args)
           result
         end
       end
       
       def wrap_method(subject_method, &block)
         define_method(:_arbitrary_code, &block)
-        
-        alias_method "original_#{normalize_method_name(subject_method)}", subject_method
         
         define_method subject_method do |*args|
           _arbitrary_code(*args)
